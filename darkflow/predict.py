32bcae88977df54fd31e8e669df44d574df35334
ColeDavis99 <coledavis6499@gmail.com>
2021-04-10 07:32:58 -0500

    Still training for kangaroos or soccerballs

--- darkflow/predict.py
+++ darkflow/predict.py
@@ -1,9 +1,32 @@
 import cv2
 from darkflow.net.build import TFNet
 import matplotlib.pyplot as plt
+import numpy as np
 
-#This is an ipython command, whatever that means.
-#%config InlineBackend.figure_format = 'svg'
+
+def pred_img(options):
+    #This loads pre-trained parameters from the checkpoint that we just specified in options.
+    tfnetPredict = TFNet(options)
+    tfnetPredict.load_from_ckpt()
+
+    #We will take one of the images that has not been used for training to predict the class, bounding box and confidence
+    original_img = cv2.imread("testimgs/soccer2.jpg")
+
+    original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
+    result = tfnetPredict.return_predict(original_img)
+    print(result)
+
+    # pull out some info from the results
+    for i in range(0, len(result)):
+        tl = (result[i]['topleft']['x'], result[i]['topleft']['y'])
+        br = (result[i]['bottomright']['x'], result[i]['bottomright']['y'])
+        label = result[i]['label']  # add the box and label and display it
+        img = cv2.rectangle(original_img, tl, br, (0, 255, 0), 7)
+        img = cv2.putText(
+            img, label, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+    plt.imshow(original_img)
+    plt.show()
+
 
 
 '''
@@ -17,34 +40,11 @@ options = {
     'model': 'cfg/model_608_coco/yolov2-1c.cfg',
     
     'backup': 'ckpt/',
-    'load': -1,           #Which training checkpoint should this model be loaded from (needs something like tiny-yolo-10.meta to work)
+    'load': 2600,           #Which training checkpoint should this model be loaded from (needs something like tiny-yolo-10.meta to work)
     
-    'threshold': 0.03,   #0.3 default confidence
+    'threshold': 0.0011,   #0.3 default confidence
     # 'gpu': 0.45
 }
 
-
-#This loads pre-trained parameters from the checkpoint that we just specified in options.
-tfnetPredict = TFNet(options)
-tfnetPredict.load_from_ckpt()
-
-#We will take one of the images that has not been used for training to predict the class, bounding box and confidence
-#original_img = cv2.imread("testimg1.jpg")
-original_img = cv2.imread("testimg2.jpg")
-# original_img = cv2.imread("testimg4.jpg")
-
-original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
-result = tfnetPredict.return_predict(original_img)
-print(result)
-
-# pull out some info from the results
-for i in range(0, len(result)):
-    tl = (result[i]['topleft']['x'], result[i]['topleft']['y'])
-    br = (result[i]['bottomright']['x'], result[i]['bottomright']['y'])
-    label = result[i]['label']  # add the box and label and display it
-    img = cv2.rectangle(original_img, tl, br, (0, 255, 0), 7)
-    img = cv2.putText(img, label, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
-plt.imshow(original_img)
-plt.show()
-
-print("WUB")
+#Time to predict whats in an image
+pred_img(options)
--- darkflow/train.py
+++ darkflow/train.py
@@ -2,8 +2,6 @@ import cv2
 from darkflow.net.build import TFNet
 import matplotlib.pyplot as plt
 
-#This is an ipython command, whatever that means.
-#%config InlineBackend.figure_format = 'svg'
 
 
 '''
@@ -13,18 +11,18 @@ import matplotlib.pyplot as plt
 '''
 #Some variables for training the model
 options = { "model": "cfg/model_608_coco/yolov2-1c.cfg",     #Training the 1 class model     
-            "load": "bin/yolov2-coco-608x608.weights",   #Start training on original weights
-            # "load": -1,     #Resume loading from most recently trained weights
-            "batch": 8,      #Oringially 2
-            "epoch": 100,     #Originally 5
+            # "load": "bin/yolov2-coco-608x608.weights",   #Start training on original weights
+            "load": -1,     #Resume loading from most recently trained weights
+            "batch": 1,      #Oringially 2
+            "epoch": 20,     #Originally 5
             "train": True,
 
-            "annotation": "../Soccerball/annots/",
-            "dataset": "../Soccerball/images/",
-            #  "annotation": "../Kangaroo/annots/",
-            #  "dataset": "../Kangaroo/images/",
+            # "annotation": "../Soccerball/annots/",
+            # "dataset": "../Soccerball/images/",
+             "annotation": "../Kangaroo/annots/",
+             "dataset": "../Kangaroo/images/",
             "gpu": 1.0,
-            "save": 256       # save/batch is how many steps the model saves. 
+            "save": 175       # save/batch is how many steps the model saves. 
 
 }
 
